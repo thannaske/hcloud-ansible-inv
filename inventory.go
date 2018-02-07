2f577821d546227cd2dab134e81f4088396e26ba
Tobias <tobias@hannaske.eu>
2018-02-08 00:28:45 +0100

    Error handling and test preparation

--- inventory.go
+++ inventory.go
@@ -1,12 +1,13 @@
-package hetzner_cloud_ansible_inventory
+package hcloudinventory
 
 import (
-	"github.com/hetznercloud/hcloud-go/hcloud"
-	"github.com/Jeffail/gabs"
 	"context"
-	"fmt"
+
+	"github.com/Jeffail/gabs"
+	"github.com/hetznercloud/hcloud-go/hcloud"
 )
 
+// GetInventoryFromAPI returns a JSON-formatted and Ansible-compatible representation of all virtual servers that are listed under the specified Hetzner Cloud API account.
 func GetInventoryFromAPI(client *hcloud.Client) (json string, err error) {
 	// New JSON return object
 	jsonReturn := gabs.New()
@@ -14,8 +15,16 @@ func GetInventoryFromAPI(client *hcloud.Client) (json string, err error) {
 	// Fetch servers from Hetzner Cloud API using it's official golang API client
 	serverList, err := client.Server.All(context.Background())
 
+	// Check for errors during fetching from Hetzner API
+	if err != nil {
+		// @todo Error handling
+	}
+
 	// Prepare host array
-	jsonReturn.ArrayOfSize(len(serverList), "hetzner-cloud", "hosts")
+	_, err = jsonReturn.ArrayOfSize(len(serverList), "hetzner-cloud", "hosts")
+	if err != nil {
+		// @todo Error handling
+	}
 
 	// Iterate through the returned server list
 	for i, server := range serverList {
@@ -24,16 +33,23 @@ func GetInventoryFromAPI(client *hcloud.Client) (json string, err error) {
 		hostName := server.PublicNet.IPv4.DNSPtr
 
 		// Set meta information for the host
-		jsonReturn.Set(server.Datacenter.Name, "_meta", "hostvars", hostName, "dcName")
-		jsonReturn.Set(server.Datacenter.Location.City, "_meta", "hostvars", hostName, "dcCity")
+		_, err := jsonReturn.Set(server.Datacenter.Name, "_meta", "hostvars", hostName, "dcName")
+		if err != nil {
+			// @todo Error handling
+		}
+
+		_, err = jsonReturn.Set(server.Datacenter.Location.City, "_meta", "hostvars", hostName, "dcCity")
+		if err != nil {
+			// @todo Error handling
+		}
 
 		// Set host information
-		jsonReturn.Path("hetzner-cloud.hosts").SetIndex(hostName, i)
+		_, err = jsonReturn.Path("hetzner-cloud.hosts").SetIndex(hostName, i)
+		if err != nil {
+			// @todo Error handling
+		}
 	}
 
-	fmt.Println(jsonReturn.StringIndent("", "  "))
-
-
-	return "", nil
+	return jsonReturn.StringIndent("", "  "), nil
 
 }
